290bd6fa90424b640ac87fcf9949cb9de447dd1b
dependentmadani <madani.badaoui12@gmail.com>
2026-09-03 13:28:55 +0100

    add tests

---
+++ tests/test_vector.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_vector.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbadaoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 14:41:28 by mbadaoui          #+#    #+#             */
+/*   Updated: 2023/02/19 14:41:28 by mbadaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_vector.hpp"
+
+int main(void)
+{
+    std::cout << RED << "________________________________________________________________________________________________________" << std::endl;
+    std::cout << RED << "**** The test is taking so much time to test the all cases and the time complexity of each method ****" << std::endl;
+    std::cout << RED << "--------------------------------------------------------------------------------------------------------" << RESET << std::endl;
+    signal(SIGALRM, alarm_handler);
+    iterator_tests();
+    const_iterator_tests();
+    reverse_iterator_tests();
+    reverse_iterator_with_ft_vector();
+    vector_tests();
+    return 0;
+}
